286b49e1abb2de973a8de7da741c8be852789efe
zainMahar78 <kahansuno3@gmail.com>
2025-12-11 17:22:20 +0500

    committed on 5:21 PM 11/12/2025

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const input = document.getElementById("input");
 const divForResult = document.getElementById("divForResult");
-
+const divForPoster = document.getElementById("divForPoster");
 const searchBtn = document.getElementById("searchBtn");
+const backBtn = document.getElementById("backBtn");
 searchBtn.addEventListener("click", toShowResult);
 function toShowResult() {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=8ee2e0565d4c1fc23a59aadbfbcf2278&query=${input.value}`;
@@ -9,10 +10,12 @@ function toShowResult() {
         divForResult.innerText = "nothing";
         return;
     }
+    divForPoster.classList.add("hidden");
     fetch(url).then((res) => {
         return res.json();
     }).then((data) => {
         divForResult.innerHTML = "";
+        backBtn.classList.remove("hidden");
         console.log(data);
         console.log(data.results[0]);
         console.log();
@@ -24,9 +27,8 @@ function toShowResult() {
         <div class="card-body">
         <h5 class="card-title"><strong>${data.results[0].original_title}</strong></h5>
         <p id="colorForReleaseDate">${data.results[0].release_date}</p>
-        <p class="card-text">${data.results[0].overview}</p>
-        
-  </div>`;
+        <p class="card-text overView">${data.results[0].overview}</p>
+        </div>`;
             divForResult.appendChild(divForCadsToDisplayResult);
             console.log();
             console.log(data.results[0].overview);
