55a87c73377001d9435054020aece6005aaab8fb
zainMahar78 <kahansuno3@gmail.com>
2025-12-24 02:07:46 +0500

    committed on 2:07 PM 24/12/2025

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const divForPoster = document.getElementById("divForPoster");
 const searchBtn = document.getElementById("searchBtn");
 const backBtn = document.getElementById("backBtn");
 const divForFullInfo = document.getElementById("divForFullInfo");
+let currentView;
 searchBtn.addEventListener("click", toShowResult);
 function toShowResult() {
+  currentView = "result";
   const url = `https://api.themoviedb.org/3/search/movie?api_key=8ee2e0565d4c1fc23a59aadbfbcf2278&query=${input.value}`;
   if (input.value === "") {
     divForResult.innerText = "nothing";
@@ -20,45 +22,56 @@ function toShowResult() {
       divForResult.innerHTML = "";
       backBtn.classList.remove("hidden");
       divForResult.classList.remove("hidden");
-      const poster_path = data.results[0].poster_path;
-      const backdrop_path = data.results[0].backdrop_path;
-      const divForCadsToDisplayResult = document.createElement("div");
-      divForCadsToDisplayResult.classList.add(
-        "card",
-        "d-flex",
-        "flex-row",
-        "cardDiv"
-      );
-      divForCadsToDisplayResult.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${poster_path}" class="card-img-top movie_img">
+      data.results.forEach((movie) => {
+        const poster_path = movie.poster_path;
+        const backdrop_path = movie.backdrop_path;
+        const divForCadsToDisplayResult = document.createElement("div");
+
+        divForCadsToDisplayResult.classList.add(
+          "card",
+          "d-flex",
+          "flex-row",
+          "cardDiv"
+        );
+        divForCadsToDisplayResult.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${poster_path}" class="card-img-top movie_img">
         <div class="card-body">
         <h5 class="card-title"><strong>${data.results[0].original_title}</strong></h5>
         <p id="colorForReleaseDate">${data.results[0].release_date}</p>
         <p class="card-text overView">${data.results[0].overview}</p> 
         </div>`;
 
-      divForResult.appendChild(divForCadsToDisplayResult);
-      const img = document.querySelector(".movie_img");
-      const langCode = data.results[0].original_language;
-      const langName = new Intl.DisplayNames(["en"], {type: "language"}).of(langCode); 
-      img.addEventListener("click", () => {
-        backBtn.classList.add("hidden");
-        fullInfo(poster_path, backdrop_path, data.results[0].original_title, data.results[0].release_date, data.results[0].overview, langName);
+        divForResult.appendChild(divForCadsToDisplayResult);
+        const img = document.querySelector(".movie_img");
+        const langCode = movie.original_language;
+        const langName = new Intl.DisplayNames(["en"], { type: "language" }).of(
+          langCode
+        );
+        img.addEventListener("click", () => {
+          fullInfo(
+            poster_path,
+            backdrop_path,
+            data.results[0].original_title,
+            data.results[0].release_date,
+            data.results[0].overview,
+            langName
+          );
+        });
       });
     });
 }
-backBtn.addEventListener("click", () => {
-  divForPoster.classList.remove("hidden");
+function fullInfo(
+  poster_path,
+  backdrop_path,
+  data,
+  release_date,
+  overView,
+  langName
+) {
+  currentView = "fullInfo";
   divForResult.classList.add("hidden");
-  backBtn.classList.add("hidden");
-});
-function fullInfo(poster_path, backdrop_path, data, release_date, overView, langName) {
-  divForResult.classList.add("hidden");
-  
   divForFullInfo.classList.remove("hidden");
-  backBtn.classList.remove("hidden");
   divForFullInfo.innerHTML = "";
   divForFullInfo.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${backdrop_path})`;
- 
   const imgWrapper = document.createElement("div");
   imgWrapper.classList.add("imgWrapper");
   const img = document.createElement("img");
@@ -68,13 +81,22 @@ function fullInfo(poster_path, backdrop_path, data, release_date, overView, lang
   const divText = document.createElement("div");
   divText.classList.add("divText");
   const h1 = document.createElement("h1");
-  h1.innerText = data+"("+release_date.split("-")[0]+")";
+  h1.innerText = data + "(" + release_date.split("-")[0] + ")";
   divText.appendChild(h1);
   const p = document.createElement("p");
   p.innerHTML = `<p>${release_date}</br><strong>Original Language: </strong>${langName}</p><h5>Overview</h5>${overView}`;
-  
+
   divText.appendChild(p);
   divForFullInfo.appendChild(divText);
-  
-  document.body.appendChild(divForFullInfo);
 }
+backBtn.addEventListener("click", () => {
+  if (currentView === "result") {
+    divForResult.classList.add("hidden");
+    divForPoster.classList.remove("hidden");
+    backBtn.classList.add("hidden");
+  } else if (currentView === "fullInfo") {
+    divForFullInfo.classList.add("hidden");
+    divForResult.classList.remove("hidden");
+    currentView = "result";
+  }
+});
